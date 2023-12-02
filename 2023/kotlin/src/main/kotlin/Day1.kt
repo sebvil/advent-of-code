a06e9ec25b3e044a82c31829a5b9c11af872dc2d
sebvil <>
2023-12-02 15:03:09 -0800

    Added check test input utils

--- 2023/kotlin/src/main/kotlin/Day1.kt
+++ 2023/kotlin/src/main/kotlin/Day1.kt
@@ -1,14 +1,32 @@
 import utils.*
 
-data object Day1 : Day<List<Day1.Input>>() {
+data object Day1 : Day<List<Day1.Input>> {
+    data class Input(val digits: List<String>)
 
     override val day: Int = 1
-    data class Input(val digits: List<String>)
+    override val part1TestInput: String = """
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+    """.trimIndent()
+    override val part2TestInput: String = """
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+    """.trimIndent()
+    override val part1Expected: Number = 142
+    override val part2Expected: Number = 281
+
     override fun parseFile(file: List<String>): List<Input> {
         val digits =
             (1..9).map { it.toString() } + Digits.entries.filter { it != Digits.Zero }.map { it.name.lowercase() }
         return file.map { line ->
-            instanceFromRegex<Input>(
+            instanceFromRegex(
                 text = line,
                 regex = regex(Regex(pattern = "(.*)"))
                 {
@@ -17,14 +35,14 @@ data object Day1 : Day<List<Day1.Input>>() {
         }
     }
 
-    override fun part1(): Number {
+    override fun part1(input: List<Input>): Number {
         return input.sumOf { row ->
             val digits = row.digits.filter { it[0].isDigit() }
             "${digits.first()}${digits.last()}".toInt()
         }
     }
 
-    override fun part2(): Number {
+    override fun part2(input: List<Input>): Number {
         return input.sumOf { row ->
             val digits = row.digits.map {
                 if (it[0].isDigit()) it else enumFromString<Digits>(it).ordinal
--- 2023/kotlin/src/main/kotlin/Day2.kt
+++ 2023/kotlin/src/main/kotlin/Day2.kt
@@ -1,60 +1,60 @@
-import utils.*
-
-data object Day2 : Day<List<Day2.Game>>() {
-    data class Cube(val count: Int, val color: Color)
-    enum class Color {
-        Red,
-        Green,
-        Blue
-    }
-
-    data class CubeSet(val cubes: List<Cube>) {
-        fun isInvalid(): Boolean {
-            return cubes.map {
-                when (it.color) {
-                    Color.Red -> it.count > 12
-                    Color.Green -> it.count > 13
-                    Color.Blue -> it.count > 14
-                }
-            }.any { it }
-        }
-    }
-
-    data class Game(val id: Int, val cubeSets: List<CubeSet>) {
-        fun isInvalid(): Boolean {
-            return cubeSets.map { it.isInvalid() }.any { it }
-        }
-    }
-
-
-    override val day: Int = 2
-    override fun parseFile(file: List<String>): List<Game> {
-        return file.map { line ->
-            instanceFromRegex<Game>(
-                text = line,
-                regex = Regex(pattern = "Game ([0-9]*): (.*)")
-            ) {
-                regex(regex = Regex(pattern = "(.*?;|.+)")) {
-                    regex(Regex(pattern = "([0-9]*) (red|green|blue)")) forGroup 0
-                } forGroup 1
-            }
-
-        }
-    }
-
-    override fun part1(): Number {
-        return input.filter { game ->
-            !game.isInvalid()
-        }.sumOf { it.id }
-    }
-
-    override fun part2(): Number {
-        return input.sumOf { game ->
-            game.cubeSets.maxOf { cubeSet ->
-                cubeSet.cubes.find { it.color == Color.Red }?.count ?: 0
-            } * game.cubeSets.maxOf { cubeSet ->
-                cubeSet.cubes.find { it.color == Color.Blue }?.count ?: 0
-            } * game.cubeSets.maxOf { it.cubes.find { cubeSet -> cubeSet.color == Color.Green }?.count ?: 0 }
-        }
-    }
-}
+//import utils.*
+//
+//data object Day2 : Day<List<Day2.Input>> {
+//    data class Cube(val count: Int, val color: Color)
+//    enum class Color {
+//        Red,
+//        Green,
+//        Blue
+//    }
+//
+//    data class CubeSet(val cubes: List<Cube>) {
+//        fun isInvalid(): Boolean {
+//            return cubes.map {
+//                when (it.color) {
+//                    Color.Red -> it.count > 12
+//                    Color.Green -> it.count > 13
+//                    Color.Blue -> it.count > 14
+//                }
+//            }.any { it }
+//        }
+//    }
+//
+//    data class Input(val id: Int, val cubeSets: List<CubeSet>) {
+//        fun isInvalid(): Boolean {
+//            return cubeSets.map { it.isInvalid() }.any { it }
+//        }
+//    }
+//
+//
+//    override val day: Int = 2
+//    override fun parseFile(file: List<String>): List<Input> {
+//        return file.map { line ->
+//            instanceFromRegex(
+//                text = line,
+//                regex = Regex(pattern = "Game ([0-9]*): (.*)")
+//            ) {
+//                regex(regex = Regex(pattern = "(.*?;|.+)")) {
+//                    regex(Regex(pattern = "([0-9]*) (red|green|blue)")) forGroup 0
+//                } forGroup 1
+//            }
+//
+//        }
+//    }
+//
+//    override fun part1(): Number {
+//        return input.filter { game ->
+//            !game.isInvalid()
+//        }.sumOf { it.id }
+//    }
+//
+//    override fun part2(): Number {
+//        return input.sumOf { game ->
+//            game.cubeSets.maxOf { cubeSet ->
+//                cubeSet.cubes.find { it.color == Color.Red }?.count ?: 0
+//            } * game.cubeSets.maxOf { cubeSet ->
+//                cubeSet.cubes.find { it.color == Color.Blue }?.count ?: 0
+//            } * game.cubeSets.maxOf { it.cubes.find { cubeSet -> cubeSet.color == Color.Green }?.count ?: 0 }
+//        }
+//    }
+//}
