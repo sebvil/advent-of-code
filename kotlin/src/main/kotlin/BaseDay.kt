bdd6abc904e1cc28bf35bea597f1395e445d5ccd
sebvil <8863909+sebvil@users.noreply.github.com>
2023-12-03 16:16:09 -0800

    extract common Day logic

---
+++ kotlin/src/main/kotlin/BaseDay.kt
@@ -0,0 +1,34 @@
+import utils.readInput
+
+interface BaseDay<Input> {
+    private val input: Input
+        get() = parseFile(readInput(year, day))
+
+    val year: Int
+    val day: Int
+    val part1TestInput: String
+    val part2TestInput: String
+    val part1Expected: Number
+    val part2Expected: Number
+    fun parseFile(file: List<String>): Input
+
+    fun part1(input: Input): Number
+    fun part2(input: Input): Number
+
+    fun checkPart1() {
+        val result = part1(parseFile(part1TestInput.lines()))
+        check(result == part1Expected) {
+            "Part 1 failed. Got $result, expected: $part1Expected"
+        }
+    }
+
+    fun checkPart2() {
+        val result = part2(parseFile(part2TestInput.lines()))
+        check(result == part2Expected) {
+            "Part 2 failed. Got $result, expected: $part2Expected"
+        }
+    }
+
+    fun part1(): Number = part1(input)
+    fun part2(): Number = part2(input)
+}
--- kotlin/src/main/kotlin/y23/Day.kt
+++ kotlin/src/main/kotlin/y23/Day.kt
@@ -1,36 +1,8 @@
 package y23
 
-import utils.readInput
+import BaseDay
 
-sealed interface Day<Input> {
-    private val input: Input
-        get() = parseFile(readInput(23, day))
-
-
-    val day: Int
-    val part1TestInput: String
-    val part2TestInput: String
-    val part1Expected: Number
-    val part2Expected: Number
-    fun parseFile(file: List<String>): Input
-
-    fun part1(input: Input): Number
-    fun part2(input: Input): Number
-
-    fun checkPart1() {
-        val result = part1(parseFile(part1TestInput.lines()))
-        check(result == part1Expected) {
-            "Part 1 failed. Got $result, expected: $part1Expected"
-        }
-    }
-
-    fun checkPart2() {
-        val result = part2(parseFile(part2TestInput.lines()))
-        check(result == part2Expected) {
-            "Part 2 failed. Got $result, expected: $part2Expected"
-        }
-    }
-
-    fun part1(): Number = part1(input)
-    fun part2(): Number = part2(input)
+sealed interface Day<Input> : BaseDay<Input> {
+    override val year: Int
+        get() = 23
 }
